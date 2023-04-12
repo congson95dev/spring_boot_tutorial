64821446febb43fcb38f661c0df9fefb7decdcd4
fudo <saxsax199502a@gmail.com>
2023-04-12 17:22:33 +0700

    change spring boot version to 2.7.10 and java version to 11

--- src/main/java/com/example/tutorial/TutorialApplication.java
+++ src/main/java/com/example/tutorial/TutorialApplication.java
@@ -2,8 +2,13 @@ package com.example.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// import spring-boot-starter-security package will allow the spring boot project homepage redirect to the /login page
+// and it will prevent you to access all of API
+// so to avoid this, we will need to remove the security package from pom.xml
+// or we can add "exclude = { SecurityAutoConfiguration.class }"
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TutorialApplication {
 
 	public static void main(String[] args) {
